1e84a31994ad3de051438afca4fc199643dc3e22
Francesco Franchina <cescus92@gmail.com>
2018-09-26 00:25:58 +0100

    Added the function to print matrices

--- lib/prettyprint.h
+++ lib/prettyprint.h
@@ -6,3 +6,13 @@ void print_array(double *buffer, int len) {
         printf("%2.2f ", buffer[i]);
     printf("]\n");
 }
+
+void print_matrix(double *buffer, int size) {
+    printf("\n");
+    for (int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+            if (j < size - 1)
+                printf("%2.2f ", buffer[i * size + j]);
+            else
+                printf("%2.2f\n", buffer[i * size + j]);
+}
