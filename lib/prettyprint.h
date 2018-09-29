4bfcc570dbaf6716e13e47484e3c583ad15d5650
Francesco Franchina <cescus92@gmail.com>
2018-09-29 20:40:55 +0100

    Added the printing of an adjacency matrix

--- lib/prettyprint.h
+++ lib/prettyprint.h
@@ -16,3 +16,19 @@ void print_matrix(double *buffer, int size) {
             else
                 printf("%2.2f\n", buffer[i * size + j]);
 }
+
+void print_adjm(short *buffer, int size) {
+    printf("\n  ");
+    for (int i = 0; i < size; i++) printf("_%2d", i);
+    printf("\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("%2d|", i);
+
+        for(int j = 0; j < size; j++)
+            if (j < size - 1)
+                printf("%2d ", buffer[i * size + j]);
+            else
+                printf("%2d\n", buffer[i * size + j]);
+    }
+}
